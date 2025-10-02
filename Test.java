06acbe32106d8880e0be715b4672f898704d5581
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-10-02 14:34:42 +0700

    user 2 merge lagi

--- Test.java
+++ Test.java
@@ -1,8 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hi, ini baris kode pertama yang dimodifikasi User 1");
-        System.out.println("User 1 menambahkan baris baru");
-        System.out.println("user 1 coba merge");
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi User 2");
+        System.out.println("User 2 menambahkan baris baru");
     }
 }
